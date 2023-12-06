05ddb21cde9b8abf4e965b4edf356679af91d614
mattdegods <matthewcpfeifer@gmail.com>
2023-12-06 16:47:12 -0500

    Finish vault_close

--- cluster1/src/vault_close.ts
+++ cluster1/src/vault_close.ts
@@ -14,9 +14,15 @@ import {
 } from "@project-serum/anchor";
 import { WbaVault, IDL } from "../programs/wba_vault";
 import wallet from "../wba-wallet.json";
+import bs58 from "bs58";
 
 // Import our keypair from the wallet file
-const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
+const base58PrivateKey = wallet;
+// decode the base58 private key to a buffer
+const buffer = bs58.decode(base58PrivateKey);
+// convert the buffer to a Uint8Array
+const uint8ArrayPrivateKey = new Uint8Array(buffer);
+const keypair = Keypair.fromSecretKey(new Uint8Array(uint8ArrayPrivateKey));
 
 // Commitment
 const commitment: Commitment = "confirmed";
@@ -30,25 +36,36 @@ const provider = new AnchorProvider(connection, new Wallet(keypair), {
 });
 
 // Create our program
-const program = new Program<WbaVault>(IDL, "<address>" as Address, provider);
+const program = new Program<WbaVault>(
+  IDL,
+  "D51uEDHLbWAxNfodfQDv7qkp8WZtxrhi3uganGbNos7o" as Address,
+  provider
+);
 
-// Create a random keypair
-const vaultState = new PublicKey("<address>");
+// Grab initialized account
+const vaultState = new PublicKey(
+  "HMEbWDGeENF5g3pothkiQefp8mTm7QA8szSAB9CHmqJp"
+);
 
-// Create a random keypair
-// const closeVaultState = ???
+const closeVaultState = new PublicKey(
+  "HMEbWDGeENF5g3pothkiQefp8mTm7QA8szSAB9CHmqJp"
+);
 
 (async () => {
   try {
-    // const signature = await program.methods
-    // .closeAccount()
-    // .accounts({
-    //     ???
-    // })
-    // .signers([
-    //     keypair
-    // ]).rpc();
-    // console.log(`Close success! Check out your TX here:\n\nhttps://explorer.solana.com/tx/${signature}?cluster=devnet`);
+    const signature = await program.methods
+      .closeAccount()
+      .accounts({
+        owner: keypair.publicKey,
+        vaultState,
+        systemProgram: SystemProgram.programId,
+        closeVaultState: closeVaultState,
+      })
+      .signers([keypair])
+      .rpc();
+    console.log(
+      `Close success! Check out your TX here:\n\nhttps://explorer.solana.com/tx/${signature}?cluster=devnet`
+    );
   } catch (e) {
     console.error(`Oops, something went wrong: ${e}`);
   }
